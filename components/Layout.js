a935c1310e4756b80a06cdf0789342518562f14f
Miguel Aguilar Torrez <aguilartorrezmiguel@gmail.com>
2023-09-27 21:22:36 -0400

    agregando..

--- components/Layout.js
+++ components/Layout.js
@@ -8,7 +8,7 @@ const Layout = ({ children }) => {
   // Verificar si la página actual es la de inicio de sesión
   const isLoginPage = router.pathname === '/login';
 
-  // Mostrar el Navbar solo en las páginas que no sean la de inicio de sesión deeee
+  // Mostrar el Navbar solo en las páginas que no sean la de inicio de sesión
   if (isLoginPage) {
     return <main>{children}</main>;
   }
